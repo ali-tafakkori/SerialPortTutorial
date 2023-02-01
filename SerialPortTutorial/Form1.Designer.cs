ed58471de89250bd6dd41a8bfc321c5474e5341d
Ali <49212265+ali-tafakkori@users.noreply.github.com>
2023-02-01 13:46:34 +0330

    init

---
+++ SerialPortTutorial/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SerialPortTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(480, 104);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 20);
+            this.tbInput.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(493, 147);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lbData
+            // 
+            this.lbData.AutoSize = true;
+            this.lbData.Location = new System.Drawing.Point(523, 285);
+            this.lbData.Name = "lbData";
+            this.lbData.Size = new System.Drawing.Size(35, 13);
+            this.lbData.TabIndex = 2;
+            this.lbData.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbData);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lbData;
+    }
+}
+
---
+++ SerialPortTutorial/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System;
+
+
+namespace SerialPortTutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static SerialPort _serialPort;
+
+        private void si_DataReceived(string data) { lbData.Text = data.Trim(); }
+
+        private delegate void SetTextDeleg(string text);
+
+
+        void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = _serialPort.ReadLine();
+       
+            this.BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { data });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           _serialPort = new SerialPort("COM16", 19200, Parity.None, 8, StopBits.One);
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+            _serialPort.Open();
+
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string text = tbInput.Text;
+            _serialPort.Write(text + "\r\n");
+        }
+
+    }
+}
